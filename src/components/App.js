d90fc2dae5551e32719792f1999a3a2a47db70f5
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-07 17:52:14 -0400

    Filter dropdown and submit button working

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,34 @@ class App extends React.Component {
     };
   }
 
+  onChangeType = (event) => {
+
+    console.log("New Filter: " + event.target.value)
+    this.setState(
+      {filters:
+        {...this.state.filters,
+          type: event.target.value
+        }
+      }
+    )
+  }
+
+  onFindPetsClick = (event) => {
+    event.preventDefault()
+    if (this.state.filters.type === 'all') {
+      fetch('/api/pets')
+      .then(res => res.json())
+      .then(res => console.log(res))
+    }
+    else {
+      const url = '/api/pets?type=' + this.state.filters.type
+      fetch(url)
+      .then(res => res.json())
+      .then(res => console.log(res))
+    }
+  }
+
+
   render() {
     return (
       <div className="ui container">
@@ -25,7 +53,7 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
               <PetBrowser />
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,22 +5,26 @@ class Filters extends React.Component {
     super();
   }
 
+  // I added <form> tags below per react js docs
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
-        <div className="field">
-          <select name="type" id="type">
-            <option value="all">All</option>
-            <option value="cat">Cats</option>
-            <option value="dog">Dogs</option>
-            <option value="micropig">Micropigs</option>
-          </select>
-        </div>
 
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
-        </div>
+          <form onSubmit={this.props.onFindPetsClick}>
+            <div className="field">
+              <select onChange={this.props.onChangeType} name="type" id="type">
+                <option value="all">All</option>
+                <option value="cat">Cats</option>
+                <option value="dog">Dogs</option>
+                <option value="micropig">Micropigs</option>
+              </select>
+            </div>
+
+            <div className="field">
+              <button className="ui secondary button">Find pets</button>
+            </div>
+        </form>
       </div>
     );
   }
